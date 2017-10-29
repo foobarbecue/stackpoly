c9ef132272e9db6a00e5ab99a45fbd01736b9ce8
Aaron Curtis <foobarbecue@gmail.com>
2017-10-28 22:27:46 -0700

    Cache results to disk to save on API quota limits (and other progress)

--- stackpoly.py
+++ stackpoly.py
@@ -1,5 +1,6 @@
 import requests
 import pandas
+from joblib import Memory
 import ipdb
 high_reps_responses = []
 net_ids_responses = []
@@ -9,6 +10,15 @@ count_filter = '!*Mx9qn.DLTGmNI0t'
 user_id_only_filter = '!T6o*9ZK8_erLEeMPOT'
 key = 'wk8Ekfg)gRCUZqy6gDJ6rQ(('
 
+fn_cache = Memory(cachedir='cache')
+
+@fn_cache.cache
+def get_sites():
+    sites = requests.get(r"https://api.stackexchange.com/2.2/sites", params={'key':key, 'pagesize':5000})
+    return sites.json()
+
+
+@fn_cache.cache
 def get_user_ids(site = 'stackoverflow', min_rep = 10000, page = 1, key=None, filter=user_id_only_filter):
     high_reps_parameters = {'page': page, 'sort': 'reputation', 'min': min_rep, 'site': site, 'pagesize': 100,
                             "filter": filter, 'key': key}
@@ -21,6 +31,7 @@ def get_user_ids(site = 'stackoverflow', min_rep = 10000, page = 1, key=None, fi
     # ids = [item['user_id'] for item in high_reps_resp_json['items']]
     # return ids, high_reps_resp_jsons
 
+@fn_cache.cache
 def get_associated_net_users(user_ids, page=1, key=None, filter=None):
     ids_semicolon_delimited = ";".join([str(id) for id in user_ids])
     net_ids_parameters = {'pagesize':100, 'page': page, 'key': key, 'filter':filter}
@@ -29,33 +40,26 @@ def get_associated_net_users(user_ids, page=1, key=None, filter=None):
         params=net_ids_parameters)
     return net_ids_req.json()
 
-def get_all_pages_net_users(site='earthscience', min_rep=10000, key=key):
+def get_all_pages_net_users(sites=['earthscience','music'], min_rep=10000, key=key):
     page = 0
     net_users_dfs = []
-    while True:
-        page += 1
-        user_ids_json = get_user_ids(site=site, min_rep=min_rep, page=page, key=key)
-        ids = [item['user_id'] for item in user_ids_json['items']]
-        if ids:
-            net_users_page_json = get_associated_net_users(ids, key=key)
-            net_users_dfs.append(pandas.DataFrame(net_users_page_json['items']))
-        if not user_ids_json['has_more']:
-            break
+    for site in sites:
+        while True:
+            page += 1
+            user_ids_json = get_user_ids(site=site, min_rep=min_rep, page=page, key=key)
+            ids = [item['user_id'] for item in user_ids_json['items']]
+            if ids:
+                net_users_page_json = get_associated_net_users(ids, key=key)
+                net_users_dfs.append(pandas.DataFrame(net_users_page_json['items']))
+            if not user_ids_json['has_more']:
+                break
     return pandas.concat(net_users_dfs, ignore_index=True)
-
-def plot_num_polymaths(net_users_df):
-    import seaborn
-    user_sites = net_users_df.pivot(index='account_id', columns='site_name', values='reputation')
-    (user_sites > 300).sum(axis='columns')
 
 def plot_corr_mat(net_users_df):
     import seaborn
     user_sites = net_users_df.pivot(index='account_id', columns='site_name', values='reputation')
     corr_mat = user_sites.fillna(0).corr()
-    seaborn.heatmap(corr_mat, xticklabels=corr_mat.columns.values, yticklabels=corr_mat.columns.values)
-    # net_ids_combined = get_net_ids()
-    # user_sites = net_ids_combined.pivot(index='account_id', columns='site_name', values='reputation')
-    # correlation_matrix = user_sites.fillna(0).corr()
-    # xticks(rotation = 90)
-    # yticks(rotation = 0)
-    # pandas.concat(net_ids_responses, ignore_index=True)
+    corr_plot = seaborn.heatmap(corr_mat, xticklabels=corr_mat.columns.values, yticklabels=corr_mat.columns.values)
+    corr_plot.set_xticklabels(corr_plot.get_xticklabels(), rotation=90)
+    corr_plot.set_yticklabels(corr_plot.get_yticklabels(), rotation=0)
+    return corr_plot
