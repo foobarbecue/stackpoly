d7008e92d47c8d2665becc3cc216c0e9cb5f1281
Aaron Curtis <foobarbecue@gmail.com>
2017-10-29 10:53:18 -0700

    Bugfix: go back to page 1 when switching sites.

--- stackpoly.py
+++ stackpoly.py
@@ -10,7 +10,7 @@ count_filter = '!*Mx9qn.DLTGmNI0t'
 user_id_only_filter = '!bWXkdAjeoQxEWi'
 key = 'wk8Ekfg)gRCUZqy6gDJ6rQ(('
 
-fn_cache = Memory(cachedir='cache')
+fn_cache = Memory(cachedir='cache', verbose=0)
 
 @fn_cache.cache
 def get_sites():
@@ -22,9 +22,9 @@ def get_sites():
 def get_user_ids(site = 'stackoverflow', min_rep = 10000, page = 1, key=None, filter=user_id_only_filter):
     high_reps_parameters = {'page': page, 'sort': 'reputation', 'min': min_rep, 'site': site, 'pagesize': 100,
                             "filter": filter, 'key': key}
-    high_reps_req = requests.get(r"https://api.stackexchange.com/2.2/users", params=high_reps_parameters)
+    user_ids_req = requests.get(r"https://api.stackexchange.com/2.2/users", params=high_reps_parameters)
     print('Got page {page} of users from {site} with reputation greater than {min_rep}'.format(**locals())) #cheeky
-    return high_reps_req.json()
+    return user_ids_req.json()
 
     #
     # high_reps_responses.append(high_reps_resp_json)
@@ -44,25 +44,39 @@ def get_accounts(user_ids, key=None, filter=None):
             params=account_ids_parameters)
         account_ids_json = account_ids_req.json()
         user_accounts += account_ids_json['items']
+        print("got {} site user ids from associated stackexchange network accounts".format(len(user_accounts)))
         if not account_ids_json['has_more']:
             break
-    ipdb.set_trace()
     return account_ids_json
 
-def get_all_pages_net_users(sites=['earthscience','music'], min_rep=1000, key=key):
-    page = 0
+@fn_cache.cache
+def get_all_linked_users(sites=['earthscience','music'], min_rep=1000, key=key):
+    """
+    :param sites: StackExchange network sites on which to being user search (e.g. 'stackoverflow' or 'earthscience')
+    :param min_rep: Minimum reputation above which to include user in results
+    :param key: Application API key (raises request limit from 300 / day to 10000 / day)
+    :return: Pandas DataFrame containing basic user info for:
+                A. All user accounts matching the parameters requested on the sites requested.
+                B. The same user accounts on all other StackExchange sites which are linked to the accounts found in A.
+    """
     net_users_dfs = []
     for site in sites:
+        page = 0
         while True:
             page += 1
             user_ids_json = get_user_ids(site=site, min_rep=min_rep, page=page, key=key)
-            user_ids = [item['account_id'] for item in user_ids_json['items']]
-            if user_ids:
-                net_users_page_json = get_accounts(user_ids, key=key)
+            user_ids_list = [item['account_id'] for item in user_ids_json['items']]
+            if user_ids_list:
+                net_users_page_json = get_accounts(user_ids_list, key=key)
                 net_users_dfs.append(pandas.DataFrame(net_users_page_json['items']))
             if not user_ids_json['has_more']:
                 break
+    ipdb.set_trace()
     return pandas.concat(net_users_dfs, ignore_index=True)
+
+def site_rep_by_net_id(*args, **kwargs):
+    user_site_reps = get_all_linked_users(*args, **kwargs)
+    return user_site_reps.pivot(index='account_id', columns='site_name', values='reputation')
 
 def plot_num_polymaths(net_users_df):
     user_site_reps = net_users_df.pivot(index='account_id', columns='site_name', values='reputation')
